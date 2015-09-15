fe4a3fc0cdbbbb0a4fee69120cda039e2fd79a79
peter11295 <r04631025@ntu.edu.tw>
2015-09-15 23:40:56 +0800

    ver2

--- debug/moc_mainwindow.cpp
+++ debug/moc_mainwindow.cpp
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[11];
-    char stringdata0[289];
+    QByteArrayData data[12];
+    char stringdata0[320];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -39,7 +39,8 @@ QT_MOC_LITERAL(6, 121, 36), // "on_red_horizontalSlider_value..."
 QT_MOC_LITERAL(7, 158, 38), // "on_green_horizontalSlider_val..."
 QT_MOC_LITERAL(8, 197, 37), // "on_blue_horizontalSlider_valu..."
 QT_MOC_LITERAL(9, 235, 26), // "on_blur_pushButton_clicked"
-QT_MOC_LITERAL(10, 262, 26) // "on_save_pushButton_clicked"
+QT_MOC_LITERAL(10, 262, 26), // "on_save_pushButton_clicked"
+QT_MOC_LITERAL(11, 289, 30) // "on_negative_pushButton_clicked"
 
     },
     "MainWindow\0on_open_pushButton_clicked\0"
@@ -49,7 +50,8 @@ QT_MOC_LITERAL(10, 262, 26) // "on_save_pushButton_clicked"
     "on_green_horizontalSlider_valueChanged\0"
     "on_blue_horizontalSlider_valueChanged\0"
     "on_blur_pushButton_clicked\0"
-    "on_save_pushButton_clicked"
+    "on_save_pushButton_clicked\0"
+    "on_negative_pushButton_clicked"
 };
 #undef QT_MOC_LITERAL
 
@@ -59,7 +61,7 @@ static const uint qt_meta_data_MainWindow[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       8,   14, // methods
+       9,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -67,14 +69,15 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   54,    2, 0x08 /* Private */,
-       3,    1,   55,    2, 0x08 /* Private */,
-       5,    0,   58,    2, 0x08 /* Private */,
-       6,    1,   59,    2, 0x08 /* Private */,
-       7,    1,   62,    2, 0x08 /* Private */,
-       8,    1,   65,    2, 0x08 /* Private */,
-       9,    0,   68,    2, 0x08 /* Private */,
-      10,    0,   69,    2, 0x08 /* Private */,
+       1,    0,   59,    2, 0x08 /* Private */,
+       3,    1,   60,    2, 0x08 /* Private */,
+       5,    0,   63,    2, 0x08 /* Private */,
+       6,    1,   64,    2, 0x08 /* Private */,
+       7,    1,   67,    2, 0x08 /* Private */,
+       8,    1,   70,    2, 0x08 /* Private */,
+       9,    0,   73,    2, 0x08 /* Private */,
+      10,    0,   74,    2, 0x08 /* Private */,
+      11,    0,   75,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void,
@@ -83,6 +86,7 @@ static const uint qt_meta_data_MainWindow[] = {
     QMetaType::Void, QMetaType::Int,    4,
     QMetaType::Void, QMetaType::Int,    4,
     QMetaType::Void, QMetaType::Int,    4,
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
 
@@ -103,6 +107,7 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 5: _t->on_blue_horizontalSlider_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
         case 6: _t->on_blur_pushButton_clicked(); break;
         case 7: _t->on_save_pushButton_clicked(); break;
+        case 8: _t->on_negative_pushButton_clicked(); break;
         default: ;
         }
     }
@@ -133,13 +138,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 8)
+        if (_id < 9)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 8;
+        _id -= 9;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 8)
+        if (_id < 9)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 8;
+        _id -= 9;
     }
     return _id;
 }
